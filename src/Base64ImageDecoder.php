48ac8adc6f0cba36782f492e433ffdff8d88b67b
Marcelo Moura <marcelofmoura2@gmail.com>
2018-07-21 14:49:23 -0300

    add false to formats create base64imageencoder

--- src/Base64ImageDecoder.php
+++ src/Base64ImageDecoder.php
@@ -5,8 +5,8 @@ namespace Melihovv\Base64ImageDecoder;
 use Melihovv\Base64ImageDecoder\Exceptions\InvalidFormat;
 use Melihovv\Base64ImageDecoder\Exceptions\NotBase64Encoding;
 
-class Base64ImageDecoder
-{
+class Base64ImageDecoder {
+
     /**
      * @var string
      */
@@ -27,41 +27,43 @@ class Base64ImageDecoder
      */
     private $content;
 
-    public function __construct(string $base64EncodedImage, array $allowedFormats = ['jpeg', 'png', 'gif'])
-    {
-        $this->base64EncodedImage = $base64EncodedImage;
+    /**
+     * 
+     * @param string $base64EncodedImage
+     * @param type $allowedFormats
+     */
+    public function __construct(string $base64EncodedImage, $allowedFormats = ['jpeg', 'png', 'gif']) {
+        $this->base64EncodedImage = str_replace('data://', 'data:', $base64EncodedImage);
         $this->allowedFormats = $allowedFormats;
 
         $this->validate();
     }
 
-    private function validate()
-    {
+    private function validate() {
         $parts = explode(',', $this->base64EncodedImage);
         $this->format = str_replace(['data:image/', ';', 'base64'], ['', '', ''], $parts[0] ?? '');
         $this->content = $parts[1] ?? '';
 
-        if (! preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $this->content)) {
+        if (!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $this->content)) {
             throw NotBase64Encoding::create();
         }
-
-        if (! in_array($this->format, $this->allowedFormats, true)) {
-            throw InvalidFormat::create($this->allowedFormats, $this->format);
+        if ($this->allowedFormats) {
+            if (!in_array($this->format, $this->allowedFormats, true)) {
+                throw InvalidFormat::create($this->allowedFormats, $this->format);
+            }
         }
     }
 
-    public function getFormat() : string
-    {
+    public function getFormat(): string {
         return $this->format;
     }
 
-    public function getContent() : string
-    {
+    public function getContent(): string {
         return $this->content;
     }
 
-    public function getDecodedContent() : string
-    {
+    public function getDecodedContent(): string {
         return base64_decode($this->content);
     }
+
 }
